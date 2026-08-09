f99134f1ef10d1b08be05cd91097375c877e3855
Busra2456 <you@example.com>
2026-08-09 18:01:50 +0600

    fix: my bookings route

--- src/middleware.js
+++ src/middleware.js
@@ -1,27 +1,18 @@
-// import { NextResponse } from "next/server";
-// // import {cookies} from "next/headers";
-// export const middleware = async (request) =>{
-//       const token = request.cookies.get("next-auth.session-token");
-//       const pathname = request.nextUrl.pathname
-//       if(pathname.includes('api')){
-//             return NextResponse.next();
-//       }
-//       if (!token){
-//             return NextResponse.redirect(new URL(`/login?redirect=${pathname}`,request.url));
-
-//       }
-//       return NextResponse.next();
-// };
-// export const config = {
-//       matcher: ["/my-bookings/:path","/services/:path"]
-// }
-
 import { NextResponse } from "next/server";
+// import {cookies} from "next/headers";
+export const middleware = async (request) =>{
+      const token = request.cookies.get("next-auth.session-token");
+      const pathname = request.nextUrl.pathname
+      if(pathname.includes('api')){
+            return NextResponse.next();
+      }
+      if (!token){
+            return NextResponse.redirect(new URL(`/login?redirect=${pathname}`,request.url));
 
-export const middleware = async (request) => {
-  return NextResponse.next();
+      }
+      return NextResponse.next();
 };
-
 export const config = {
-  matcher: [],
-};
+      matcher: ["",""]
+}
+
