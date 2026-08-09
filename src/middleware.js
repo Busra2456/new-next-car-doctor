cc9a03b7dfbd8901d3ab7ba95dcd7949fcf5788e
Busra2456 <you@example.com>
2026-08-09 17:47:02 +0600

    services

--- src/middleware.js
+++ src/middleware.js
@@ -13,5 +13,5 @@ export const middleware = async (request) =>{
       return NextResponse.next();
 };
 export const config = {
-      matcher: ["/my-bookings/:path*"]
+      matcher: ["/my-bookings/:path","/services/:path"]
 }
